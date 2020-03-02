7e50ad6f3228f24b9abc7d01d24a58785f57d552
domin <domin@LAPTOP-V8VF4B7N>
2020-03-02 02:20:06 -0500

    First Commit

---
+++ Downloads/sandbox-01/src/main/java/com/sandbox/sandbox01/Sandbox01Application.java
@@ -0,0 +1,13 @@
+package com.sandbox.sandbox01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sandbox01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sandbox01Application.class, args);
+	}
+
+}
